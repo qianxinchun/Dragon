fd938f5f199c2e4439143a09c2e91935248c0ea5
Neo <641763345@qq.com>
2016-03-13 19:59:33 +0800

    remove del-dir commond

--- examples/cifar10/cmd_cifar.cmd
+++ examples/cifar10/cmd_cifar.cmd
@@ -1,7 +1,4 @@
 set DATA_DIR=G:/Dataset/cifar10/DATA
 set DB_DIR=G:/Dataset/cifar10
-set DB_DIR_DEL=G:\Dataset\cifar10
-rd /s /q %DB_DIR_DEL%\cifar10_train_lmdb
-rd /s /q %DB_DIR_DEL%\cifar10_test_lmdb
 start /wait convert_cifar.exe %DATA_DIR%  %DB_DIR% lmdb
 start compute_mean.exe -backend=lmdb %DB_DIR%/cifar10_train_lmdb  %DB_DIR%/mean.binary
